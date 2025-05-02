61908b3bd4bea04f79c13f337b5ec53a57d058c7
lesaintcode <rjunqueira@tulane.edu>
2025-05-01 19:00:04 -0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,3 +5,8 @@ from main import *
 ## Feel free to add your own tests here.
 def test_multiply():
     assert quadratic_multiply(BinaryNumber(3), BinaryNumber(3)) == 3*3
+    assert quadratic_multiply(BinaryNumber(9), BinaryNumber(11)) == 99
+    assert quadratic_multiply(BinaryNumber(14), BinaryNumber(2)) == 28
+    assert quadratic_multiply(BinaryNumber(13), BinaryNumber(13)) == 169
+    assert quadratic_multiply(BinaryNumber(1), BinaryNumber(123)) == 123
+    assert quadratic_multiply(BinaryNumber(5), BinaryNumber(20)) == 100
